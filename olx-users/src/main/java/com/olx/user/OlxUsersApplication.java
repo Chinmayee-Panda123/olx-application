2cbc629cd784018a134cf791b51c3b9d9b66dd7b
CP113584 <CP113584@AESW42SLPC5818.ind.zensar.com>
2025-01-14 17:53:24 +0530

    olx-user project created

---
+++ olx-users/src/main/java/com/olx/user/OlxUsersApplication.java
@@ -0,0 +1,13 @@
+package com.olx.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxUsersApplication.class, args);
+	}
+
+}
